4fe85e7478f93ea568019dc92b9fd900c1669a75
Dainius Urbonas <fordainius@gmail.com>
2019-05-23 12:33:02 +0300

    Done 8kyu kata

---
+++ kyu8/kyu82018-05-23.js
@@ -0,0 +1,13 @@
+//Very simple, given a number, find its opposite.
+
+
+//My solution
+function opposite(number) {
+  //your code here
+  return number - (2* number)
+}
+
+//Best practices
+function opposite(number) {
+  return(-number);
+}
